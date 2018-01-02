31ee76993e6ff5993311a322011815f7380b528a
Brent Scarbrough <BrentScarbrough@jst-fcx3p32.limitedtoo.com>
2018-01-02 15:24:58 -0500

    Init

---
+++ Snapchat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Snapchat
+//
+//  Created by Brent Scarbrough on 1/2/18.
+//  Copyright © 2018 Brent Scarbrough. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
